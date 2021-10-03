1691dcf51384efd72bad2e059dc59830b82d654a
Nicolas Alonso <63936366+projectAlonso@users.noreply.github.com>
2021-10-03 23:33:09 +0300

    Update plusMinus.js

--- plusMinus.js
+++ plusMinus.js
@@ -5,11 +5,14 @@
 
 function plusMinus(arr) {
     // Write your code here
+    // Variables to serve as counters for how many possitive, negative, or 'zeroes' are in the array
     let possitives = 0;
     let negatives = 0;
     let zeroes = 0;
+    // Variable to know how many numbers there are in the array
     const total = arr.length;
     
+    // Iterator to count the total number of possitives, negatives, and 'zeroes' in the array
     arr.forEach(num => {
         if (num > 0) {
             possitives++;
@@ -20,9 +23,11 @@ function plusMinus(arr) {
         }
     })
     
+    // Variables to calculate the percentage/rate of possitives/negatives/'zeroes' present in the array (with 6 decimals)
     const possitiveRate = (possitives / total).toFixed(6);
     const negativeRate = (negatives / total).toFixed(6);
     const zeroRate = (zeroes / total).toFixed(6);
+    // Results logged in 3 different lines
     console.log(parseFloat(possitiveRate)); 
     console.log(parseFloat(negativeRate));
     console.log(parseFloat(zeroRate));
