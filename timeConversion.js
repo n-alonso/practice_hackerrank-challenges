c082b74e42cd4607280a3c0aa6db38b82a467c73
Nicolas Alonso <63936366+n-alonso@users.noreply.github.com>
2021-10-05 14:14:25 +0300

    Update timeConversion.js

--- timeConversion.js
+++ timeConversion.js
@@ -2,6 +2,33 @@
 Given a time in 12-hour AM/PM format, convert it to military (24-hour) time. 
  */
 
+/*
+REFACTORED
+*/
+function timeConversion(string) {
+  // Write your code here
+  // Separate the format (AM/PM) and the hour which will need to change from the minutes and seconds
+  const format = string.slice(-2).toLowerCase();
+  const array = string.split(':');
+  let hour = array[0];
+  const minutesSeconds = array[1].concat(':', (array[2].slice(0, 2)));
+   
+  // Return the appropriate military hour
+  switch (format) {
+    case 'am':
+      hour = hour === '12' ? '00' : hour;
+      break;
+    case 'pm':
+      hour = hour === '12' ? '12' : (parseInt(hour)+12).toString();
+  }
+  
+  // Build the result hours+minutes+seconds minus format
+  return hour.concat(':', minutesSeconds);
+}
+
+/*
+ORIGINAL
+*/
 function timeConversion(s) {
     // Write your code here
     // Re-usable function to sum 12 if it is 'PM' and delete the AM/PM at the end
